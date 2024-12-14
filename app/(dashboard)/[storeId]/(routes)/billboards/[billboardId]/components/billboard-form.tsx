2f4567c2c26341685499d6ac93fd662480ffa83b
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-12-14 19:01:22 +0000

    Remove unused imports from the billboard form component

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -16,8 +16,6 @@ import { Separator } from '@/components/ui/separator';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import AlertModal from '@/components/modals/alert-modal';
-import ApiAlert from '@/components/ui/api-alert';
-import { useOrigin } from '@/hooks/use-origin';
 import ImageUpload from '@/components/ui/image-upload';
 
 const fromSchema = z.object({
@@ -34,7 +32,6 @@ interface Props {
 const BillboardForm: React.FC<Props> = ({ initialData }) => {
 	const params = useParams();
 	const router = useRouter();
-	const origin = useOrigin();
 
 	const [open, setOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
